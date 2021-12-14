f398ec1543826036604679452614f9853983564b
Yessine Ben Hlima <87872940+BHYessine@users.noreply.github.com>
2021-12-14 20:32:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-// app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/public/index.html"));
-// });
-
-// app.get("/services", (req, res) => {
-//     res.sendFile(__dirname, + "/public/services.html");
-// })
-
-// app.get("/style", (req, res) => {
-//     res.sendFile(__dirname, + "/public/style.css");
-// })
 const date = new Date(Date.now());
 const day = date.getDay();
 const hour = date.getHours();
